35004688a1dfda071696fd7273602ffa99cf687e
anithagutha <anithagutha9948@gmail.com>
2018-01-18 18:53:36 +0530

    see this code & change madi

--- app/src/main/java/com/example/sreekanthp/evokepower/MainActivity.java
+++ app/src/main/java/com/example/sreekanthp/evokepower/MainActivity.java
@@ -2,21 +2,40 @@ package com.example.sreekanthp.evokepower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    EditText  et1,et2,et3;
+    Button  btn,btn2,btn3;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-///kjnnkjhjkikjjkj
-        //check  dix ciode mb
-        //..,;l,;l,,l;l
-        //hj
-        //helloo raman
-        //he,kljfhtfghfuf
-      // anita see this codee
-        //Lavanya change some madification in this code
         setContentView(R.layout.activity_main);
+        et1= (EditText) findViewById(R.id.et1);
+        et2= (EditText) findViewById(R.id.et2);
+        et3= (EditText) findViewById(R.id.et2);
+        btn= (Button) findViewById(R.id.btn);
+        btn2= (Button) findViewById(R.id.btn2);
+        btn3= (Button) findViewById(R.id.btn3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/sreekanthp/evokepower/Register.java
@@ -0,0 +1,8 @@
+package com.example.sreekanthp.evokepower;
+
+/**
+ * Created by India on 18-Jan-18.
+ */
+
+public class Register  {
+}
